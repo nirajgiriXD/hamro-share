975bff1ef1a9987c5fe97ccc3e1c52a946f82be3
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-01-21 16:55:10 +0545

    Add functionality to apply for share

--- utilities/apply.js
+++ utilities/apply.js
@@ -1,6 +1,4 @@
-const { applicableCompanies } = require("../data/applicableCompanies");
-
-const apply = async (page, userData) => {
+const apply = async (page, userData, applicableCompanies) => {
   // Go to the ASBA page.
   await page.click("i.msi-asba");
 
@@ -12,51 +10,96 @@ const apply = async (page, userData) => {
   const companies = await page.$$(".company-list");
 
   let count = 0;
-  let applyBtn;
-  let companyName;
-
   for (const company of companies) {
     count++;
-    companyName = await page.evaluate((el) => {
-      return el.querySelector(".company-name > span:nth-child(1)")?.textContent;
-    }, company);
-    companyName = companyName.trim() ?? "Null";
 
-    if (applicableCompanies.includes(companyName)) {
+    const companyName = await page.evaluate((el) => {
+      return (
+        el
+          .querySelector(".company-name > span:nth-child(1)")
+          ?.textContent?.trim() ?? "Null"
+      );
+    }, company);
+
+    const applyBtnTxt = await page.evaluate(
+      (el, count) => {
+        return (
+          el
+            .querySelector(`button.btn-issue:nth-child(${count})`)
+            ?.textContent?.trim() ?? "Null"
+        );
+      },
+      company,
+      count
+    );
+
+    if (applicableCompanies.includes(companyName) && applyBtnTxt === "Apply") {
       // Click apply button.
       await page.click(`button.btn-issue:nth-child(${count})`);
 
       // Wait for navigation to complete
       await page.waitForNavigation();
 
-      // Select Bank.
-      await page.click("#selectBank");
-      await page.click("#selectBank > option:nth(2)");
+      // Wait for the search results page to load.
+      await page.waitForSelector("#disclaimer", {
+        timeout: 5000,
+      });
 
-      // Select Account Number.
+      // Select First Bank in the Options.
+      await page.click("#selectBank");
+      const banks = await page.evaluate(() => {
+        return Array.from(
+          document.querySelectorAll("#selectBank > option")
+        ).map((option) => ({
+          text: option.textContent.trim(),
+          value: option.value,
+        }));
+      });
+      await page.select("#selectBank", banks[1].value);
+
+      // Wait for the search results page to load.
+      await page.waitForSelector("#accountNumber", {
+        timeout: 5000,
+      });
+
+      // Select First Account Number in the Options.
       await page.click("#accountNumber");
-      await page.click("#accountNumber > option:nth(2)");
+      const accountNumbers = await page.evaluate(() => {
+        return Array.from(
+          document.querySelectorAll("#accountNumber > option")
+        ).map((option) => ({
+          text: option.textContent.trim(),
+          value: option.value,
+        }));
+      });
+      await page.select("#accountNumber", accountNumbers[1].value);
 
       // Enter the applied kitta number.
-      await page.$eval("#appliedKitta", (el) => (el.value = 10));
+      await page.type("#appliedKitta", "10");
 
       // Enter CRN number.
-      await page.$eval("#crnNumber", (el) => (el.value = userData.crn));
+      await page.type("#crnNumber", userData.crn);
 
       // Check the disclaimer.
       await page.click("#disclaimer");
 
-      // Click submit.
+      // // Click submit.
       await page.click('.card-footer button[type="submit"]');
 
-      // Wait for navigation to complete.
-      await page.waitForNavigation();
+      // Wait for the search results page to load.
+      await page.waitForSelector("#transactionPIN", {
+        timeout: 5000,
+      });
 
       // Enter the pin number.
-      await page.$eval("#transactionPIN", (el) => (el.value = userData.pin));
+      await page.focus("#transactionPIN");
+      await page.type("#transactionPIN", userData.pin);
 
       // Click submit.
-      await page.click('.confirm-page-btn button[type="submit"]');
+      // await page.click('.confirm-page-btn button[type="submit"]');
+
+      // Wait for navigation to complete
+      // await page.waitForNavigation();
     }
   }
 };
